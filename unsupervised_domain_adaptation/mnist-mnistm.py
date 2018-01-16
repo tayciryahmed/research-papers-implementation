398d59f5d91cfed22e7220e8dcc9adeaf269cdcc
Taycir Yahmed <taycir.yahmed@gmail.com>
2018-01-16 12:23:59 +0100

    update params

--- unsupervised_domain_adaptation/mnist-mnistm.py
+++ unsupervised_domain_adaptation/mnist-mnistm.py
@@ -6,6 +6,7 @@ from utils import *
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
 # Process MNIST
 mnist_train = (mnist.train.images > 0).reshape(55000, 28, 28, 1).astype(np.float32)
 mnist_train = np.concatenate([mnist_train, mnist_train, mnist_train], 3)
@@ -14,7 +15,7 @@ mnist_test = np.concatenate([mnist_test, mnist_test, mnist_test], 3)
 
 
 # Load MNIST-M
-mnistm = pkl.load(open('mnistm_data.pkl')) #https://drive.google.com/file/d/1dylmd8Hp0gldo4lJUq2r-8eeqSyQAJBr/view?usp=sharing
+mnistm = pkl.load(open('../ATDA/mnistm_data.pkl'))
 mnistm_train = mnistm['train']/255.
 mnistm_test = mnistm['test']/255.
 mnistm_valid = mnistm['valid']/255.
@@ -24,9 +25,10 @@ model = ATDA()
 model.fit_ATDA(X_source_train=mnist_train, y_source_train=mnist.train.labels,
                        X_target_test=mnistm_test, y_target_test=mnist.test.labels,
                        X_target_train=mnistm_train, y_target_train=mnist.train.labels,
-                      threshold=0.9, n_epoch=5, k=100, lr=0.01, batch_size_F1F2=64, batch_size_Ft=128)
+                      threshold=0.9, n_epoch=5, k=20, lr=0.01, batch_size_F1F2=64, batch_size_Ft=128)
 
 """
+for a quick test: 
 model.fit_ATDA(X_source_train=mnist_train[:100, :], y_source_train=mnist.train.labels[:100],
                        X_target_test=mnistm_test[:50, :], y_target_test=mnist.test.labels[:50],
                        X_target_train=mnistm_train[:100, :], y_target_train=mnist.train.labels[:100],
