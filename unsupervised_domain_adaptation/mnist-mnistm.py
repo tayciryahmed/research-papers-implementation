8b86cc4ee35d1b963b2565a0496254c80b22c7af
Taycir Yahmed <taycir.yahmed@gmail.com>
2018-01-16 18:10:43 +0100

    Update mnist-mnistm.py

--- unsupervised_domain_adaptation/mnist-mnistm.py
+++ unsupervised_domain_adaptation/mnist-mnistm.py
@@ -5,8 +5,7 @@ from ATDA import ATDA
 from utils import *
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
+mnist = input_data.read_data_sets('../ATDA/MNIST_data', one_hot=True)
 # Process MNIST
 mnist_train = (mnist.train.images > 0).reshape(55000, 28, 28, 1).astype(np.float32)
 mnist_train = np.concatenate([mnist_train, mnist_train, mnist_train], 3)
@@ -24,13 +23,12 @@ model = ATDA()
 
 model.fit_ATDA(X_source_train=mnist_train, y_source_train=mnist.train.labels,
                        X_target_test=mnistm_test, y_target_test=mnist.test.labels,
-                       X_target_train=mnistm_train, y_target_train=mnist.train.labels,
-                      threshold=0.9, n_epoch=5, k=20, lr=0.01, batch_size_F1F2=64, batch_size_Ft=128)
+                       X_target_train_valid=mnistm_train, y_target_train_valid=mnist.train.labels,
+                      threshold=0.9, n_epoch=5, k=100, lr=0.01, batch_size_F1F2=64, batch_size_Ft=128)
 
 """
-for a quick test: 
-model.fit_ATDA(X_source_train=mnist_train[:100, :], y_source_train=mnist.train.labels[:100],
-                       X_target_test=mnistm_test[:50, :], y_target_test=mnist.test.labels[:50],
-                       X_target_train=mnistm_train[:100, :], y_target_train=mnist.train.labels[:100],
+model.fit_ATDA(X_source_train=mnist_train[:1000, :], y_source_train=mnist.train.labels[:1000],
+                       X_target_test=mnistm_test[:1000, :], y_target_test=mnist.test.labels[:1000],
+                       X_target_train_valid=mnistm_train[:1000, :], y_target_train_valid=mnist.train.labels[:1000],
                       threshold=0, n_epoch=1, k=1, batch_size_F1F2=1, batch_size_Ft=1, lr=0.01)
 """
